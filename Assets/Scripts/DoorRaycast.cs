fc0dd08e10d74c4de47440d9d76be3fdeddf96ba
ryzze89 <94044488+ryzze89@users.noreply.github.com>
2022-01-26 00:28:12 +0000

    door script and controller

---
+++ Assets/Scripts/DoorRaycast.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorRaycast : MonoBehaviour
+{
+   [SeralizeField] Private int rayLength = 5;
+   [SeralizeField] Private Layer layerMaskInteract;
+   [SeralizeField] Private string excludeLayerName = null;
+
+   private MyDoorController raycastedObj;
+
+   [SeralizeField] private KeyCode opeDoorKey = KeyCode.Mouse0;  
+
+   [SerializeField] private Image crosshair = null;
+}
