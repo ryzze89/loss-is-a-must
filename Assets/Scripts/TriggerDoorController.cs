65311cb5e9fb8952a14229156b78ea898775cff6
ryzze89 <94044488+ryzze89@users.noreply.github.com>
2022-01-27 10:42:15 +0000

    door animation

---
+++ Assets/Scripts/TriggerDoorController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoorController : MonoBehaviour
+{
+  [SerializeField] private Animator myDoor = null;
+
+  [SerializeField] private bool openTrigger = false;
+  [SerializeField] private bool closeTrigger = false;
+
+    [SerializeField] private string doorOpen = "DoorOpen";
+    [SerializeField] private string doorClose = "DoorClose";
+
+    private void OnTriggerEnter(Collider Other)
+    {
+        if (Other.CompareTag("Player"))
+        {
+            if (openTrigger)
+            {
+                if (openTrigger)
+                {
+                    myDoor.Play("DoorOpen", 0, 0.0f);
+                    gameObject.SetActive(false);
+                }
+                else if (closeTrigger)
+                {
+                    myDoor.Play("DoorClose",0, 0.0f);
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+
+    }
+}
