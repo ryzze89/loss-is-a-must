a88a48b0f61b6bbc416bffda4a8cc061cd079c2d
ryzze89 <94044488+ryzze89@users.noreply.github.com>
2022-01-31 12:42:02 +0000

    dialogue

--- Assets/Dialogue.cs
+++ Assets/Dialogue.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dialogue : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+[System.Serializable]
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class Dialogue 
+{
+    public string name;
+
+    [TextArea(3, 10) ]
+    public string[] sentences;
+    
 }
