5c32e0d5013f576bd6d1767760d0675b057b673d
ryzze89 <94044488+ryzze89@users.noreply.github.com>
2022-02-01 15:57:25 +0000

    portal system

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -1,15 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
-    [SerializeField] Portal targetPortal;
+    [SerializeField] string portalID;
+    [SerializeField] string targetPortalID;
+    Portal targetPortal;
     [SerializeField] Transform outPoint;
+    [SerializeField] string portalType;
+
+    void Start()
+    {
+        GameObject[] portals = GameObject.FindGameObjectsWithTag("Portal");
+        if (portalType == "teleporter")
+        {
+            targetPortal = Array.Find(portals, p => p.GetComponent<Portal>().portalID == targetPortalID).GetComponent<Portal>();
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" && portalType == "teleporter")
         {
             other.gameObject.GetComponent<CharacterController>().enabled = false;
 
